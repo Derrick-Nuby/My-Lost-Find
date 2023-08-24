d43253e660fbc94e39c7362f165bb1fc114ab5f4
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-08-24 20:01:32 +0200

    just found out

--- pages/pages.js
+++ pages/pages.js
@@ -23,16 +23,11 @@ createBtn.addEventListener("click", () => {
 
 // javascript for form validation
 let errorDiv = document.getElementById("error");
-// const body = document.getElementsByTagName("body")[0];
-
-// window.addEventListener("load", function () {
-//   errorDiv.style.display = "none";
-// });
 
 function validateForm(event) {
   event.preventDefault();
   const emailValue = document.getElementById("emailInput").value;
-
+  const password = document.getElementById("password").value;
   function validateEmail(emailValue) {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailPattern.test(emailValue);
@@ -49,7 +44,24 @@ function validateForm(event) {
       errorDiv.style.display = "none";
     }, 5000);
   }
+  function validatePassword(password) {
+    const passwordPattern =
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&()[\]{}<>])[A-Za-z\d@$!%*?&()[\]{}<>]{8,}$/;
+    return passwordPattern.test(password);
+  }
+
+  if (validatePassword(password)) {
+    console.log("Valid password!");
+  } else {
+    errorDiv.innerText +=
+      "\n\n\nInvalid password. Please make your password strong by including 8 characters, capital letters and symbols";
+    errorDiv.style.display = "block";
+
+    setTimeout(() => {
+      errorDiv.style.display = "none";
+    }, 5000);
+  }
 }
 
-// const submitButton = document.getElementById("submitButton");
-// submitButton.addEventListener("click", validateForm);
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", validateForm);
