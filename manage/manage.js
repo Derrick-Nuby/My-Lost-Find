7d1828524f1dc7175641a3f21abcd449ab2c0d1e
Derrick-Nuby <iradukundaderrick7@gmail.com>
2023-08-31 08:37:00 +0200

    added the popups

--- manage/manage.js
+++ manage/manage.js
@@ -13,3 +13,93 @@ function menuClose() {
     side.style.display = "none";
   }, 2000);
 }
+
+// popup
+
+const modalContent = document.getElementById("modal-content");
+const modal = document.getElementById("simpleModal");
+// const modalBtn = document.getElementById("modalBtn");
+const modalBtns = document.querySelectorAll(".modalBtn");
+const closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+modalBtns.forEach((modalBtn) => {
+  modalBtn.addEventListener("click", openModal);
+});
+
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", clickOutside);
+
+// modalBtn.addEventListener("click", openModal);
+
+function openModal() {
+  modal.style.animation = "modalOpen 1s";
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.style.animation = "modalClose 2s";
+  setTimeout(function () {
+    modal.style.display = "none";
+    modalContent.innerHTML = "";
+  }, 1000);
+}
+
+function clickOutside(e) {
+  if (e.target == modal) {
+    modal.style.animation = "modalClose 2s";
+    setTimeout(function () {
+      modal.style.display = "none";
+      modalContent.innerHTML = "";
+    }, 1000);
+  }
+}
+
+function displayItem() {
+  const htmlContent = `<div class="singlelost">
+                    <span style="padding-right: 20px;" class="closeBtn" onclick="closeModal()">&times;</span>
+                <div class="title sbtnsc">Lost Item ID1987... </div>
+                <div class="contents">
+                    <table id="tables">
+                        <tr>
+                            <td>Item Type</td>
+                            <td>Identification Card</td>
+                        </tr>
+                        <tr>
+                            <td>Item Number</td>
+                            <td>1801230296644121</td>
+                        </tr>
+                        <tr>
+                            <td>Location Lost</td>
+                            <td>Kigali-Gasabo-Gacuriro</td>
+                        </tr>
+                        <tr>
+                            <td>Date Lost</td>
+                            <td>09/01/2023</td>
+                        </tr>
+                        <tr>
+                            <td>Date Reported</td>
+                            <td>19/01/2023</td>
+                        </tr>
+                        <tr>
+                            <td>Status</td>
+                            <td>Found</td>
+                        </tr>
+                        <tr>
+                            <td>Date Found</td>
+                            <td>12/01/2023</td>
+                        </tr>
+                        <tr>
+                            <td>Location Found</td>
+                            <td>Kigali-Gasabo-Gacuriro</td>
+                        </tr>
+                        <tr>
+                            <td>Acquired</td>
+                            <td>Yes</td>
+                        </tr>
+                    </table>
+
+                </div>
+                <div class="footeritem">Further Processes</div>
+            </div>`;
+  modalContent.innerHTML = htmlContent;
+}
