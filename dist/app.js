3da859da52722ab229c3f7716587ab087654bc2e
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-05-02 15:03:21 +0200

    setup

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,20 @@
-"use strict";
-console.log("hello world");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/My-Lost-Find';
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+mongoose
+    .connect(uri)
+    .then(() => app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
+    .catch(error => {
+    throw error;
+});
+export default app;
 //# sourceMappingURL=app.js.map
